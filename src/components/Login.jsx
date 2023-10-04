62d280a24f8072ccd1fa301642330925756c2cee
mansourhamidi94 <MansourHamidi@hotmail.dk>
2023-10-04 14:53:04 +0200

    login, login.css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,25 +3,57 @@ import { Button, Form, FormGroup, Label, Input } from 'reactstrap'; // Bootstrap
 import './Login.css'; // Importing the CSS for styling
 
 const Login = () => {
-    return (
-        <div className="login-container">
-            <img src="/path-to-your-logo.png" alt="Logo" className="logo" /> {/* Logo */}
-            <Form className="login-form">
-                <FormGroup>
-                    <Label for="username">Username</Label>
-                    <Input type="text" name="username" id="username" placeholder="Enter your username" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="password">Password</Label>
-                    <Input type="password" name="password" id="password" placeholder="Enter your password" />
-                </FormGroup>
-                <Button color="primary">Login</Button>
-                <div className="signup-link">
-                    Don't have a password? <a href="/signup">Sign Up</a>
-                </div>
-            </Form>
-        </div>
-    );
-}
+            return (
+                <section className="vh-100">
+                    <div className="container py-5 h-100">
+                        <div className="row d-flex align-items-center justify-content-center h-100">
+                            <div className="col-md-8 col-lg-7 col-xl-6">
+                                <img src="tidsbanken.jpg"
+                                    className="img-fluid" alt="Phone image" />
+                            </div>
+                            <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                                <form>
+                                    {/* Email input */}
+                                    <div className="form-outline mb-4">
+                                        <input type="email" id="form1Example13" className="form-control form-control-lg" />
+                                        <label className="form-label" htmlFor="form1Example13">Username</label>
+                                    </div>
+        
+                                    {/* Password input */}
+                                    <div className="form-outline mb-4">
+                                        <input type="password" id="form1Example23" className="form-control form-control-lg" />
+                                        <label className="form-label" htmlFor="form1Example23">Password</label>
+                                    </div>
+        
+                                    <div className="d-flex justify-content-around align-items-center mb-4">
+                                        {/* Checkbox */}
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="checkbox" value="" id="form1Example3" defaultChecked />
+                                            <label className="form-check-label" htmlFor="form1Example3"> Remember me </label>
+                                        </div>
+                                        <a href="#!">Forgot password?</a>
+                                    </div>
+        
+                                    {/* Submit button */}
+                                    <button type="submit" className="btn btn-primary btn-lg btn-block">Sign in</button>
+        
+                                    <div className="divider d-flex align-items-center my-4">
+                                        <p className="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+                                    </div>
+        
+                                    <a className="btn btn-primary btn-lg btn-block" style={{backgroundColor: "#3b5998"}} href="#!" role="button">
+                                        <i className="fab fa-facebook-f me-2"></i>Continue with Facebook
+                                    </a>
+                                    <a className="btn btn-primary btn-lg btn-block" style={{backgroundColor: "#55acee"}} href="#!" role="button">
+                                        <i className="fab fa-twitter me-2"></i>Continue with Twitter
+                                    </a>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            );
+        }
+        
 
 export default Login;

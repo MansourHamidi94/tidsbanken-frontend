ae497b882d007ccd2441924e6bafa86f31b8cd02
mansourhamidi94 <MansourHamidi@hotmail.dk>
2023-10-09 12:37:05 +0200

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 // import logo from './logo.svg';
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Calendar from "./components/calendar/Calendar.jsx"
-
-
+import SignUp from './components/SignUp/Register';
+import { BrowserRouter, Routes, Route, NavLink, useNavigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <Calendar/>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          <Route path="/SignUp" element={<SignUp />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

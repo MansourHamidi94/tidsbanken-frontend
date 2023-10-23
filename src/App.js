84b57ec4e8108380ac1f839ed11c82ac83de83c1
Eijs-FullStack <53994213+Eijs666@users.noreply.github.com>
2023-10-23 10:40:49 +0200

    Admin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
 import ControlPanel from "./components/controlPanel/ControlPanel.jsx"
-import Login from './components/Login/Login.jsx';
+import Login from './components/login/Login.jsx';
 import Calendar from "./components/calendar/Calendar"
 import SignUp from './components/SignUp/Signup';
 import Profile from './components/profile/Profile';
-import Admin from "./components/admin/Admin";
 import VacationRequest from './components/vacationRequest/VacationRequest.jsx';
 
 import { Provider } from 'react-redux';
